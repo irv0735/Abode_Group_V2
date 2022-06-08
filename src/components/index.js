1dc52733b5640f48d40e9b814fc4d44c72d6f44e
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-06-07 18:45:11 -0600

    Fixed background image and added personal backgrounds to each description component on landing page

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,2 @@
-
 export { default as Footer } from "./Footer";
-export { default as About } from "./About";
-export { default as Area } from "./Area";
 export { default as PropertyCard } from "./PropertyCard";
-
- 
